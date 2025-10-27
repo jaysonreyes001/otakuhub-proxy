390d629ba39b1e01813d0daa9b3d1bafc9c3372e
jaysonreyes001 <jaysonreyes012345@gmail.com>
2025-10-27 23:37:50 +0800

    upload

--- frontend/src/Pages/List/list.vue
+++ frontend/src/Pages/List/list.vue
@@ -11,7 +11,7 @@
                          <svg class="shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                               <path d="m15 18-6-6 6-6"></path>
                          </svg>
-                         <span>Previous</span>
+                         <span class="hidden lg:block">Previous</span>
                          </button>
                          <div class="flex items-center gap-x-1">
                               <button v-if="current_page  > 3" @click="pages(1)" type="button" class="min-h-9.5 min-w-9.5 flex justify-center items-center text-gray-800 hover:bg-gray-100 
@@ -37,7 +37,7 @@
                               </button>
                          </div>
                          <button :disabled="current_page >= list_details.totalPages ? true : false" @click="pages(current_page+=1)"  type="button" class="min-h-9.5 min-w-9.5 py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm rounded-lg text-gray-800 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10" aria-label="Next">
-                         <span>Next</span>
+                         <span class="hidden lg:block">Next</span>
                          <svg class="shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                               <path d="m9 18 6-6-6-6"></path>
                          </svg>
@@ -84,7 +84,7 @@
                          <svg class="shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                               <path d="m15 18-6-6 6-6"></path>
                          </svg>
-                         <span>Previous</span>
+                         <span class="hidden lg:block">Previous</span>
                          </button>
                          <div class="flex items-center gap-x-1">
                               <button v-if="current_page  > 3" @click="pages(1)" type="button" class="min-h-9.5 min-w-9.5 flex justify-center items-center text-gray-800 hover:bg-gray-100 
@@ -110,7 +110,7 @@
                               </button>
                          </div>
                          <button :disabled="current_page >= list_details.totalPages ? true : false" @click="pages(current_page+=1)"  type="button" class="min-h-9.5 min-w-9.5 py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm rounded-lg text-gray-800 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10" aria-label="Next">
-                         <span>Next</span>
+                         <span class="hidden lg:block">Next</span>
                          <svg class="shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                               <path d="m9 18 6-6-6-6"></path>
                          </svg>
